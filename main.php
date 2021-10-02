2cdeafb86906f67c06af448d265fe5473e6c36a0
michaelrdz <91229705+michaelrdz@users.noreply.github.com>
2021-10-01 20:28:30 -0500

    Página php que servira como sitio maestro, todos los modulos cargaran  desde aquí

---
+++ main.php
@@ -0,0 +1,33 @@
+<?php 
+include('includes/validation.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Proyecto GPDS</title>
+		<meta name="description" content="UTA - Sistema de prestamo">
+		<meta name="author" content="UTA">
+
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/jquery.ui.all.css" rel="stylesheet" type="text/css">
+		<script src="js/sweetalert.min.js"></script> 
+		<link rel="stylesheet" type="text/css" href="js/sweetalert.css">
+	</head>
+	<body style="background-color:green;">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12" style="padding-top:50px;">
+					
+				</div>
+			</div>
+		</div>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/scripts.js"></script>
+		<img id="background_logo" style="" src="img/logo_menu.png">
+	</body>
+</html>
